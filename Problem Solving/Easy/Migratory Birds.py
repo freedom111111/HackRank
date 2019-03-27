5a95cbf3baf290ab9ebc166dc632bb1466a838c1
freedom111111 <39920361+freedom111111@users.noreply.github.com>
2019-03-27 11:31:00 -0400

    Update Migratory Birds.py

--- Problem Solving/Easy/Migratory Birds.py
+++ Problem Solving/Easy/Migratory Birds.py
@@ -1,6 +1,4 @@
 def migratoryBirds(arr):
-    count_dict = {}
-    for x in set(arr):
-        count_dict[x] = arr.count(x)
+    count_dict = {x:arr.count(x) for x in set(arr)}
     count = sorted(count_dict, key=count_dict.get, reverse=True)    
     return count[0]
