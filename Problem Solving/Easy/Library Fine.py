8dfd40e13c596950dbd51ea072da40152233f26a
freedom111111 <39920361+freedom111111@users.noreply.github.com>
2019-03-27 16:10:52 -0400

    Create Library Fine.py

---
+++ Problem Solving/Easy/Library Fine.py
@@ -0,0 +1,17 @@
+def libraryFine(d1, m1, y1, d2, m2, y2):
+
+    if y1>y2:
+        fine = 10000
+    elif y1 == y2:
+        if m1>m2:
+            fine = 500*(m1-m2)
+        elif m1 == m2:
+            if d1>d2:
+                fine = 15*(d1-d2)
+            else:
+                fine = 0
+        else:
+            fine = 0
+    else:
+        fine = 0
+    return fine
